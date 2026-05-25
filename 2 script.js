9c5680d47b96db4f0c996e1d6928235731657e1d
Knan41 <knanbayramov41@gmail.com>
2026-05-25 16:35:42 +0400

    function declaration

---
+++ 2 script.js
@@ -0,0 +1,7 @@
+let a = prompt('reqemler yaz')
+let b = 'reqem sayi: '
+
+function reqemSayi(say) {
+    return a.length
+}
+alert(b + reqemSayi(a));
---
+++ 3 script.js
@@ -0,0 +1,9 @@
+function ededCem(cutEded){
+    let a = 0;
+    for(i = 0 ; i < cutEded ; i++){
+        if(i % 2 === 0){
+            a+=i;
+        }
+    } console.log(a);
+}
+ededCem(15)
